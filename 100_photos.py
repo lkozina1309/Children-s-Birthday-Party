8fb69d592ca09ad2f42d45fdba87834eb725310b
Luka Kozina <lukakozina6@gmail.com>
2022-01-20 14:48:47 +0100

    Add files via upload

---
+++ 100_photos.py
@@ -0,0 +1,17 @@
+import cv2
+import time
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+    for i in range(100):
+        time.sleep(3)
+        cv2.imwrite('image%s.jpg' % i, frame)
+        time.sleep(3)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+		
+cap.release()
+cv2.destroyAllWindows()
