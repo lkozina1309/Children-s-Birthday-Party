f4bdca8c1e3ceb4871bd76665cb88c1c6ae0defd
Luka Kozina <lukakozina6@gmail.com>
2022-01-21 18:01:01 +0100

    Update face_detection.py

--- src/face_detection.py
+++ src/face_detection.py
@@ -1,24 +1,30 @@
+# This script will use stream function one hundred times
+# Function will start the stream, take a photo and break after 3 seconds
+# Useful for automatic photographing
+
+# import the libraries
 import numpy as np
 import cv2
 import time
 
-
-
+# Define type of stream and load haarcascade classifier
 cap = cv2.VideoCapture(0)
 #cap = cv2.VideoCapture('http://192.168.5.16:8080/video')
-face_cascade = cv2.CascadeClassifier('/home/marija/OpenCV/haar_cascades.xml')
+face_cascade = cv2.CascadeClassifier('haar_cascades.xml')
 
+# Fuction for stream and capturing photos automatically when face is detected
 def stream(i):
     while cap.isOpened():
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 10)
-        #cv2.imshow("frame", frame)
+
         if len(faces) > 0:
             cv2.imwrite('image%s.jpg' % i, frame)  
             time.sleep(3)
             break
-		
+
+# Calling a function (change the parameter if you want more photos) 		
 for x in range(3):
     stream(x)
 
