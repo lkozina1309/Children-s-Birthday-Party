8dc9f2939e4d27d6350263270edcc088bf02efec
Luka Kozina <lukakozina6@gmail.com>
2022-01-23 22:17:42 +0100

    Update text_detection.py

--- src/text_detection.py
+++ src/text_detection.py
@@ -1,27 +1,32 @@
+# This script will use stream function one hundred times
+# Function will start the stream, take a photo when certain text (letter M here) is detected and break
+# Useful for automatic photographing
+
+
+# import the libraries
 import cv2 
 from PIL import Image
 import pytesseract
 import time
 
+# Define type of stream
 cap = cv2.VideoCapture(0)
 #cap = cv2.VideoCapture('http://192.168.5.16:8080/video')
 
 def stream(i):
     while cap.isOpened():
         ret, frame = cap.read()
-        text = pytesseract.image_to_string(frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)                     # convert to grayscale
+        text = pytesseract.image_to_string(gray)                           # extract text from an image   
         if (text=="M"):
-            cv2.imwrite('image%s.jpg' % i, frame)  
+            cv2.imwrite('image%s.jpg' % i, frame)                          # take a photo if letter M is detected
             break
         
-for x in range(3):
-    time.sleep(2)
+for x in range(100):
+    time.sleep(20)
     stream(x)
 
 cap.release()
-cv2.destroyAllWindows()
-
-
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
